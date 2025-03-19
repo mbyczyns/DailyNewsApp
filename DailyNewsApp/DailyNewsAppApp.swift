cc1284f3ff1cf4d9767fdf0fcba8538a26f8ba9e
mbyczyns <mbyczyns@gmail.com>
2025-03-19 11:21:54 +0100

    Initial Commit

---
+++ DailyNewsApp/DailyNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyNewsAppApp.swift
+//  DailyNewsApp
+//
+//  Created by Marcin Byczyński on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyNewsAppTests/DailyNewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyNewsAppTests.swift
+//  DailyNewsAppTests
+//
+//  Created by Marcin Byczyński on 19/03/2025.
+//
+
+import Testing
+@testable import DailyNewsApp
+
+struct DailyNewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
