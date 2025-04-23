29f86d746403a4a5e4d73b62ece44ac283d80a7d
mbyczyns <mbyczyns@gmail.com>
2025-04-23 14:59:02 +0200

    display list in keyword view DONE

--- DailyNewsApp/Views/KeywordsView.swift
+++ DailyNewsApp/Views/KeywordsView.swift
@@ -3,37 +3,67 @@ import SwiftUI
 struct KeywordsView: View {
     @State private var searchKeyword: String = ""
     @State private var alertItem: AlertItem?
+    @StateObject var viewModel = SnippetsListViewModel()
+    @State private var isShowingDetail = false
+    @State private var selectedSnippet: News_Snippet? = nil
 
     var body: some View {
-        
-        NavigationView {
-            VStack {
-                HStack{
-                    TextField("Enter keyword...", text: $searchKeyword)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    Button {
-                        if searchKeyword != ""{
-                            print("making a request with keyword: " +  searchKeyword)
-                        } else{
-                            alertItem = AlertContent.noKeyword
+        ZStack{
+            NavigationView {
+                VStack {
+                    HStack{
+                        TextField("Enter keyword...", text: $searchKeyword)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        Button {
+                            if searchKeyword != ""{
+                                print("making a request with keyword: " +  searchKeyword)
+                            } else{
+                                alertItem = AlertContent.noKeyword
+                            }
+                        } label: {
+                            Image(systemName: "magnifyingglass")
+                                .frame(width: 44, height: 44)
+                                .imageScale(.medium)
                         }
-                    } label: {
-                        Image(systemName: "magnifyingglass")
-                            .frame(width: 44, height: 44)
-                            .imageScale(.medium)
+                    }
+                    Text(searchKeyword)
+                    List(viewModel.snippets, id: \.title) { snippet in
+                        SnippetListCell(snippet: snippet)
+                            .onTapGesture {
+                                withAnimation {
+                                    selectedSnippet = snippet
+                                    isShowingDetail = true
+                                }
+                            }
                     }
                 }
-                Text(searchKeyword)
-                Spacer()
+                .navigationTitle("🔑 Search Keywords:")
+                .padding()
+                
             }
-            .navigationTitle("🔑 Search Keywords:")
-            .padding()
-            .alert(item: $alertItem) { alertItem in
-                            Alert(title: alertItem.title,
-                                  message: alertItem.message,
-                                  dismissButton: alertItem.dismissButton)
-                        }
+            .onAppear {
+                viewModel.getArticles()
+            }
+            .blur(radius: isShowingDetail ? 20 : 0)
+            .disabled(isShowingDetail)
+
+            
+            if isShowingDetail, let snippet = selectedSnippet {
+                Color.black.opacity(0.3)
+                    .ignoresSafeArea()
+                    .transition(.opacity)
+
+                ArticleDetailedView(article_snippet: snippet, isShowingDetail: $isShowingDetail)
+                    .transition(.scale)
+                    .zIndex(1)
+            }
         }
+        .animation(.easeInOut, value: isShowingDetail)
+        .alert(item: $alertItem) { alertItem in
+                        Alert(title: alertItem.title,
+                              message: alertItem.message,
+                              dismissButton: alertItem.dismissButton)
+                    }
     }
 }
 
