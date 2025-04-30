4d0c7cb62f17e077fa845388d822ad04e91d730c
mbyczyns <mbyczyns@gmail.com>
2025-04-30 13:16:41 +0200

    article details -> read article DONE

--- DailyNewsApp/Views/ArticleDetailedView.swift
+++ DailyNewsApp/Views/ArticleDetailedView.swift
@@ -6,86 +6,94 @@ struct ArticleDetailedView: View {
     @Binding var isShowingDetail: Bool
     
     var body: some View {
-        VStack {
-            ArticleRemoteImage(urlString: article_snippet.main_image_url)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 330, height: 210)
-            
+        NavigationStack{
             VStack {
-                Text(article_snippet.title)
-                    .padding(10)
-                    .font(.title2)
-                    .fontWeight(.medium)
-                    .frame(maxWidth: 300)
-                    .multilineTextAlignment(.center)
-                    .lineLimit(nil)
-                    .fixedSize(horizontal: false, vertical: true)
+                ArticleRemoteImage(urlString: article_snippet.main_image_url)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 330, height: 210)
                 
-                Text(article_snippet.pub_date)
-                    .font(.caption)
-                    .foregroundColor(.gray)
-                    .fontWeight(.medium)
-                
-                Text(article_snippet.print_headline)
-                    .padding(10)
-                    .font(.headline)
-                    .fontWeight(.regular)
-                    .frame(maxWidth: 350)
-                    .multilineTextAlignment(.center)
-                    .lineLimit(nil)
-                    .fixedSize(horizontal: false, vertical: true)
-                
-                Spacer()
-                
-                Button(action: { print("czytamm") }) {
-                    Text("Read now")
-                        .frame(maxWidth: .infinity)
-                        .fontWeight(.bold)
-                }
-                .buttonStyle(.borderedProminent)
-                .controlSize(.large)
-                
-            }
-        }
-        .padding(.bottom, 20)
-        .padding(.horizontal, 20)
-        .frame(width: 320, height: 500)
-        .background(Color(.systemBackground))
-        .cornerRadius(12)
-        .shadow(radius: 40)
-        .overlay(
-            HStack(spacing: 10) {
-                Button {
-                    print("downloaded article") // TODO
-                } label: {
-                    ZStack {
-                        Circle()
-                            .frame(width: 30, height: 30)
-                            .foregroundColor(.white)
-                            .opacity(0.8)
-                        Image(systemName: "square.and.arrow.down")
-                            .frame(width: 44, height: 44)
-                            .imageScale(.medium)
+                VStack {
+                    Text(article_snippet.title)
+                        .padding(10)
+                        .font(.title2)
+                        .fontWeight(.medium)
+                        .frame(maxWidth: 300)
+                        .multilineTextAlignment(.center)
+                        .lineLimit(nil)
+                        .fixedSize(horizontal: false, vertical: true)
+                    
+                    Text(article_snippet.pub_date)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                        .fontWeight(.medium)
+                    
+                    Text(article_snippet.print_headline)
+                        .padding(10)
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .frame(maxWidth: 350)
+                        .multilineTextAlignment(.center)
+                        .lineLimit(nil)
+                        .fixedSize(horizontal: false, vertical: true)
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination:
+                                    ReadArticleView(article_snippet: article_snippet)) {
+                        Text("Read now")
+                            .frame(maxWidth: .infinity)
+                            .fontWeight(.semibold)
+                            .font(.headline)
+                            .padding(0)
                     }
+                    .buttonStyle(.borderedProminent)
+                    .controlSize(.large)
+                
+                 
+                    
                 }
+            }
+            .padding(.bottom, 20)
+            .padding(.horizontal, 20)
+            .frame(width: 320, height: 500)
+            .background(Color(.systemBackground))
+            .cornerRadius(12)
+            .shadow(radius: 40)
+            .overlay(
+                HStack(spacing: 10) {
+                    Button {
+                        print("downloaded article") // TODO
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.white)
+                                .opacity(0.8)
+                            Image(systemName: "square.and.arrow.down")
+                                .frame(width: 44, height: 44)
+                                .imageScale(.medium)
+                        }
+                    }
 
-                Button {
-                    isShowingDetail = false
-                } label: {
-                    ZStack {
-                        Circle()
-                            .frame(width: 30, height: 30)
-                            .foregroundColor(.white)
-                            .opacity(0.8)
-                        Image(systemName: "xmark")
-                            .frame(width: 44, height: 44)
-                            .imageScale(.small)
+                    Button {
+                        isShowingDetail = false
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.white)
+                                .opacity(0.8)
+                            Image(systemName: "xmark")
+                                .frame(width: 44, height: 44)
+                                .imageScale(.small)
+                        }
                     }
                 }
-            }
-            .padding(8),
-            alignment: .topTrailing
-        )
+                .padding(8),
+                alignment: .topTrailing
+            )
+        }
+        
 
     }
 }
